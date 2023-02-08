717da97ac34de3e359c8d69f2cce4f7c0d6e3c44
Engel <erestrepo@cbt.es>
2023-02-08 13:30:55 +0100

    Ejer 8 Json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
 
 import { Ejer1Component } from './ejercicios/ejer1/ejer1.component';
 import { Ejer2Component } from './ejercicios/ejer2/ejer2.component';
@@ -11,6 +12,7 @@ import { Ejer3Component } from './ejercicios/ejer3/ejer3.component';
 import { Ejer4Component } from './ejercicios/ejer4/ejer4.component';
 import { Ejer5Component } from './ejercicios/ejer5/ejer5.component';
 import { Ejer6Component } from './ejercicios/ejer6/ejer6.component';
+import { Ejer7Component } from './ejercicios/ejer7/ejer7.component';
 import { Ejer8Component } from './ejercicios/ejer8/ejer8.component';
 import { Ejer9Component } from './ejercicios/ejer9/ejer9.component';
 import { Ejer10Component } from './ejercicios/ejer10/ejer10.component';
@@ -31,6 +33,7 @@ import { Ejer7Module } from './ejercicios/ejer7/ejer7.module';
     Ejer4Component,
     Ejer5Component,
     Ejer6Component,
+    Ejer7Component,
     Ejer8Component,
     Ejer9Component,
     Ejer10Component,
@@ -45,7 +48,8 @@ import { Ejer7Module } from './ejercicios/ejer7/ejer7.module';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    Ejer7Module
+    Ejer7Module,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/ejercicios/ejer7/ejer7.component.ts
@@ -0,0 +1,12 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ejer7',
+  templateUrl: './ejer7.component.html'
+})
+export class Ejer7Component {  
+
+  
+  
+}
